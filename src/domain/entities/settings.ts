c11b9ca8d4a3b65566e1e0bb1afa618ec10575d3
Nicolas Tomasi da Silva <tomasitrabalho@gmail.com>
2025-07-12 15:53:01 -0300

    .

--- src/domain/entities/settings.ts
+++ src/domain/entities/settings.ts
@@ -1,41 +1,19 @@
-import { RoleEnum } from "../enums/role-enum";
-import { Entity } from "./entity";
-
 interface SettingsProps {
-  id: string;
-  companyId: string;
-  primaryColor: string;
+  primaryColor: string | null;
   logoUrl: string | null;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt: Date | null;
 }
 
-export class Settings extends Entity {
-  private readonly _companyId;
-  private _primaryColor: string;
+export class Settings {
+  private _primaryColor: string | null;
   private _logoUrl: string | null;
   private constructor(props: SettingsProps) {
-    super({
-      id: props.id,
-      createdAt: props.createdAt,
-      updatedAt: props.updatedAt,
-      deletedAt: props.deletedAt,
-    });
-    this._companyId = props.companyId;
     this._primaryColor = props.primaryColor;
     this._logoUrl = props.logoUrl;
   }
 
-  static create(
-    props: Omit<SettingsProps, "createdAt" | "updatedAt" | "deletedAt">
-  ): Settings {
-    const now = new Date();
+  static create(props: SettingsProps): Settings {
     return new Settings({
       ...props,
-      createdAt: now,
-      updatedAt: now,
-      deletedAt: null,
     });
   }
 
@@ -45,21 +23,12 @@ export class Settings extends Entity {
 
   toJSON() {
     return {
-      id: this.id,
-      companyId: this._companyId,
       primaryColor: this._primaryColor,
       logoUrl: this._logoUrl,
-      createdAt: this.createdAt,
-      updatedAt: this.updatedAt,
-      deletedAt: this.deletedAt,
     };
   }
 
-  get companyId(): string {
-    return this._companyId;
-  }
-
-  get primaryColor(): string {
+  get primaryColor(): string | null {
     return this._primaryColor;
   }
   get logoUrl(): string | null {
